6afb70146737bba454999cac727933126fd73d56
FrancescoVassalli <Francesco.Vassalli@colorado.edu>
2018-06-14 17:28:59 -0600

    more

--- DirectphotonDijet.cc
+++ DirectphotonDijet.cc
@@ -166,7 +166,7 @@ void makeData(std::string filename, long nEvents, string pTHat, float gammaCut, 
             {
               dJTemp.setConstituents(pythiaengine.event);
               dJTemp.fill(true,&jet1size,jet1phis,jet1etas,jet1pT);
-              cout<<jet1phis[0]<<"\n";
+              cout<<jet1etas[0]<<"\n";
               dJTemp.fill(false,&jet2size,jet2phis,jet2etas,jet2pT);
               close->Fill();
             }
